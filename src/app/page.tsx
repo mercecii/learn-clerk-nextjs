5d6b5ab8782427b9fa748a878ecbcb19e5d243da
Deepak Kumar <deepakkumar@Deepaks-MacBook-Air.local>
2025-02-02 17:56:06 +0530

    build fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { auth, currentUser } from "@clerk/nextjs/server";
+import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import Dashobard from "./dashboard/page";
 
