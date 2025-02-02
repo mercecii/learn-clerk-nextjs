36da9635ccd31e1d325637cb55624808adfcff3b
Deepak Kumar <deepakkumar@Deepaks-MacBook-Air.local>
2025-02-02 15:27:11 +0530

    adding publishable key

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ClerkProvider> {children}</ClerkProvider>
+        <ClerkProvider
+          publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+        >
+          {" "}
+          {children}
+        </ClerkProvider>
       </body>
     </html>
   );
